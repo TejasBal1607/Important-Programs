d7266037ecaacdfc7d608eb556a041edf3c5f3cf
Tejas Bal <tejas1607.best@gmail.com>
2025-12-27 16:03:52 +0530

    Linked List Implementation (LIFO, FIFO, Sorted)

---
+++ C/LinkedLists.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node* next;
+} node;
+
+//LIFO Linked List Implementation
+int lifo(int num){
+    node *list = NULL;
+    for(int i=0; i<num; i++){
+        node *n = malloc(sizeof(node));
+        if (n == NULL){
+            printf("Memory allocation failed\n");
+            return -1;
+        }
+        printf("Enter data for node %d: ", i+1);
+        scanf("%d", &n->data);
+        n->next = list;
+        list = n;
+
+    }
+    printf("LIFO Linked List:\n");
+    node *ptr = list;
+    while(ptr != NULL){
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\n");
+    return 0;
+}
+
+//FIFO Linked List Implementation
+int fifo(int num){
+    node *list = NULL;
+    for(int i=0; i<num; i++){
+        node *n = malloc(sizeof(node));
+        if (n == NULL){
+            printf("Memory allocation failed\n");
+            return -1;
+        }
+        printf("Enter data for node %d: ", i+1);
+        scanf("%d", &n->data);
+        n->next = NULL;
+        if(list == NULL){
+            list = n;
+        }
+        else{
+            for(node *ptr = list; ptr != NULL; ptr = ptr->next){
+                if(ptr->next == NULL){
+                    ptr->next = n;
+                    break;
+                }
+            }
+        }
+    }
+    printf("FIFO Linked List:\n");
+    node *ptr = list;
+    while(ptr != NULL){
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+    return 0;
+}
+
+
+//Sorted Linked List Implementation
+int sortedLinkedList(int num){
+    node *list = NULL;
+    for(int i=0; i<num; i++){
+        node *n = malloc(sizeof(node));
+        if (n == NULL){
+            printf("Memory allocation failed\n");
+            return -1;
+        }
+        printf("Enter data for node %d: ", i+1);
+        scanf("%d", &n->data);
+        n->next = NULL;
+        if(list == NULL || list->data >= n->data){
+            n->next = list;
+            list = n;
+        }
+        else{
+            for(node *ptr = list; ptr != NULL; ptr = ptr->next){
+                if(ptr->next == NULL || ptr->next->data >= n->data){
+                    n->next = ptr->next;
+                    ptr->next = n;
+                    break;
+                }
+            }
+        }
+    }
+    printf("Sorted Linked List:\n");
+    node *ptr = list;
+    while(ptr != NULL){
+        printf("%d ", ptr->data);
+        ptr = ptr->next;
+    }
+    return 0;
+}
+int main(){
+    int num;
+    printf("Enter number of nodes: ");
+    scanf("%d", &num);
+    sortedLinkedList(num);
+    return 0;
+}
